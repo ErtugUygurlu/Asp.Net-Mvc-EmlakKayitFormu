1164994b2f111c8e05fe26c898ed042d9143bbff
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-15 17:00:25 +0300

    Add project files.

---
+++ KisiKayıtMVC/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KisiKayıtMVC.Models.Entity;
+
+namespace KisiKayıtMVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        KisiselBlogDBEntities db= new KisiselBlogDBEntities();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult KisiEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KisiEkle(TBLKISILER p1)
+        {
+            db.TBLKISILER.Add(p1);
+            db.SaveChanges();
+            return View(); 
+        }
+    }
+}
---
+++ KisiKayıtMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KisiKayıtMVC.MvcApplication" Language="C#" %>
